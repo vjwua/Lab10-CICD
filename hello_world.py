7d0f57f0b19f0ab5a82c9277c108531edcaa1373
vjwua <62188903+vjwua@users.noreply.github.com>
2024-12-03 19:09:47 +0200

    Initial commit

---
+++ hello_world.py
@@ -0,0 +1,5 @@
+def say_hello():
+    return "Testing bot xxx"
+
+if __name__ == "__main__":
+    print(say_hello())
---
+++ tests/test_hello_world.py
@@ -0,0 +1,4 @@
+from hello_world import say_hello
+
+def test_say_hello():
+    assert say_hello() == "Testing bot xxx"
